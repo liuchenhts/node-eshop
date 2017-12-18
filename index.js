423c8481e71f56f20c383e632b91ef1b88273d39
Chen Liu <liuchenhts@gmail.com>
2017-12-18 16:10:21 +1100

    view layout template works

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  // res.send('Hello World! ')
+  // res.sendFile(__dirname + '/index.html');
+  res.render('layout');
 })
 
 //Launch listening server on port 8081
 app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+  console.log('app listening on port 8081! ')
 })
